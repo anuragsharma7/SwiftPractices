05a3f7a98f16650dab34ea7182d58aa586d17cc3
Anurag Sharma <anurag20c@gmail.com>
2022-08-30 23:48:59 +0530

    initial commit

---
+++ SwiftPractices/SwiftPractices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPractices
+//
+//  Created by Anurag Sharma on 30/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
